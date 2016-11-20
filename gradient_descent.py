dcf71e8d03d58cdfc0286e10fa25a7936038feab
Wiliam Kudaka <wiliamkudaka@gmail.com>
2016-11-20 13:46:28 -0200

    step, sum_of_squares_gradient

--- gradient_descent.py
+++ gradient_descent.py
@@ -6,3 +6,10 @@ def sum_of_squares(v):
 
 def difference_quotient(f, x, h):
     return (f(x + h) - f(x)) / h
+
+def step(v, direction, step_size):
+    return [v_i + step_size * direction_i
+            for v_i, direction_i in zip(v, direction)]
+
+def sum_of_squares_gradient(v):
+    return [2 * v_i for v_i in v]
--- test-gradient_descent.py
+++ test-gradient_descent.py
@@ -1,5 +1,7 @@
 import gradient_descent as gd
 import unittest
+import random
+import linear_algebra as lin_alg
 
 
 class TestStatistics(unittest.TestCase):
@@ -10,8 +12,6 @@ class TestStatistics(unittest.TestCase):
         self.assertEqual(result, 29)
 
     def test_difference_quotient(self):
-
-
         def square(x):
             return x**2
         #derivada da função acima...
@@ -20,13 +20,32 @@ class TestStatistics(unittest.TestCase):
 
         derivative_estimate = lambda x: gd.difference_quotient(square, x, h=0.00001)
         derivative_estimate_list = list(map(derivative_estimate, range(-5,5)))
-        derivative_estimate_list = [round(item) for item in derivative_estimate_list]
+        derivative_estimate_list = [round(item, 2) for item in derivative_estimate_list]
 
         square_derivative_list = list(map(square_derivative, range(-5,5)))
 
         #para um h pequeno, o resultado da derivada e da função
         #'difference_quotient' são basicamente os mesmos
         self.assertEqual(derivative_estimate_list, square_derivative_list)
+
+    def test_step(self):
+        def square(x):
+            return x**2
+
+        v = list(map(square, range(-3,4)))
+
+        tolerance = 0.0000001
+
+        while True:
+            gradient = gd.sum_of_squares_gradient(v)
+            next_v = gd.step(v, gradient, -0.01)
+            if lin_alg.distance(next_v, v) < tolerance:
+                break
+            v = next_v
+
+        rounded_v = [round(item, 2) for item in v]
+        #quanto menor for a tolerância, mais perto v fica de zero
+        self.assertEqual(rounded_v, [0.0,0.0,0.0,0.0,0.0,0.0,0.0])
 
 
 if __name__ == "__main__": unittest.main()
